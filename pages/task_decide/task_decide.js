48f5ed9a89746aa3bcba5f70d400ea855a36b89d
guoyi <guoyi@kokozu.net>
2018-01-03 16:13:42 +0800

    animation

--- pages/task_decide/task_decide.js
+++ pages/task_decide/task_decide.js
@@ -1,14 +1,18 @@
 // pages/task_decide/task_decide.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    showAnimationData: {},
-    defenseViewShow: "",
-    destroyViewShow: "",
-    fireViewShow: ""
+    viewAnimationData: {},
+
+    defenseViewSub: "bottomViewHide",
+    destroyViewSub: "bottomViewHide",
+    fireViewSub: "bottomViewHide",
+
+    defenseShow: false,
+    destroyShow: false,
+    fireShow: false
   },
 
   /**
@@ -73,27 +77,95 @@ Page({
   defenseAction: function () {
     var showAnimation = wx.createAnimation({
       duration: 1000,
-      timingFunction: 'ease',
+      timingFunction: 'ease'
     })
+    
+    if (this.data.defenseShow) {
+      this.setData({
+        destroyViewSub: "bottomViewHide",
+        fireViewSub: "bottomViewHide",
+        defenseViewSub: "bottomViewHide",
+        defenseShow: false
+      })
+      showAnimation.opacity(0).step()
+    } else {
+      this.setData({
+        destroyViewSub: "bottomViewHide",
+        fireViewSub: "bottomViewHide",
+        defenseViewSub: "bottomViewShow",
+        defenseShow: true
+      })
+      showAnimation.opacity(1).step()
+    }
+
+    showAnimation.scale(1.2, 1.2).step()
+    showAnimation.rotate(180).step()
 
     this.setData({
-      defenseViewShow: "bottomViewShow"
-    })
-
-    this.showAnimation = showAnimation
-    showAnimation.opacity(1).step()
-    this.setData({
-      showAnimationData:showAnimation.export()
+      viewAnimationData:showAnimation.export()
     })
   },
 
   /// 破坏
   destroyAction: function () {
+    var showAnimation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
 
+    this.showAnimation = showAnimation
+
+    if (this.data.destroyShow) {
+      this.setData({
+        destroyViewSub: "bottomViewHide",
+        fireViewSub: "bottomViewHide",
+        defenseViewSub: "bottomViewHide",
+        destroyShow: false
+      })
+      this.showAnimation.opacity(0).step()
+    } else {
+      this.setData({
+        destroyViewSub: "bottomViewShow",
+        fireViewSub: "bottomViewHide",
+        defenseViewSub: "bottomViewHide",
+        destroyShow: true
+      })
+      this.showAnimation.opacity(1).step()
+    }
+    this.setData({
+      viewAnimationData: this.showAnimation.export()
+    })
   },
 
   /// 行动
   fireAction: function () {
+    var showAnimation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
 
+    this.showAnimation = showAnimation
+
+    if (this.data.fireShow) {
+      this.setData({
+        destroyViewSub: "bottomViewHide",
+        fireViewSub: "bottomViewHide",
+        defenseViewSub: "bottomViewHide",
+        fireShow: false
+      })
+      showAnimation.opacity(0).step()
+    } else {
+      this.setData({
+        destroyViewSub: "bottomViewHide",
+        fireViewSub: "bottomViewShow",
+        defenseViewSub: "bottomViewHide",
+        fireShow: true
+      })
+      showAnimation.opacity(1).step()
+    }
+
+    this.setData({
+      viewAnimationData: showAnimation.export()
+    })
   }
 })
